85c303906ad2e09e5c9d407621b6447d96cac08d
Minkyung Cho <112492904+miincho@users.noreply.github.com>
2024-05-02 17:19:32 -0400

    added final folder

---
+++ FINAL/index.js
@@ -0,0 +1,19 @@
+let neurotype = document.getElementById("landing-page");
+let tabs = document.getElementById("homepage");
+let typepage = document.getElementById("typesetting-page");
+let start = document.getElementById("start");
+
+document.addEventListener("DOMContentLoaded", function() {
+  //delay fade in effect
+  setTimeout(function() {
+    neurotype.style.opacity = "1";
+    tabs.style.opacity = "1";
+  }, 1000);
+  
+  //click to reveal typesetting
+  start.addEventListener("click", function() {
+    tabs.style.opacity = "0";
+    neurotype.classList.add("fade-out");
+    typepage.style.opacity = "1";
+  });
+});
